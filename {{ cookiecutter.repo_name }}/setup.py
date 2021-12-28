e9fc5152d7d6a1a1d341530e6bb1fe546bec9d0d
Victor Tolmachev <tolmachev.2406@gmail.com>
2021-12-28 18:09:58 +0300

    Add mypy config

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,9 +1,18 @@
 from setuptools import find_packages, setup
 
+
+def load_long_description():
+    with open("README.md", encoding="utf-8") as f:
+        text = f.read()
+    return text
+
+
 setup(
-    name='{{ cookiecutter.package_name }}',
+    name="{{ cookiecutter.package_name }}",
     packages=find_packages(),
-    version='0.1.0',
-    description='{{ cookiecutter.description }}',
-    author='{{ cookiecutter.author_name }}',
+    version="0.1.0",
+    author="{{ cookiecutter.author_name }}",
+    description="{{ cookiecutter.description }}",
+    long_description=load_long_description(),
+    long_description_content_type="text/markdown",
 )
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/logger.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/logger.py
@@ -11,11 +11,16 @@ LOG_LEVELS = {
 DEFAULT_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 
+def _get_library_name():
+
+    return __name__.split(".")[0]
+
+
 def configure_root_logger(log_level="INFO", fmt=DEFAULT_FORMAT):
     stream_handler = logging.StreamHandler()
     formatter = logging.Formatter(fmt)
     stream_handler.setFormatter(formatter)
-    root_logger = logging.getLogger(__package__)
+    root_logger = logging.getLogger(_get_library_name())
     root_logger.addHandler(stream_handler)
     root_logger.setLevel(LOG_LEVELS.get(log_level, logging.WARNING))
     return root_logger
